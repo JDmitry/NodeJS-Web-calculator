c8d3cf90cb1b1fdd2454be9a32cfcda922775839
Dmitry <jdmitrys@yandex.ru>
2022-10-10 12:27:46 +0400

    Working application

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const fs = require('fs');
 const url = require('url');
+const host = '127.0.0.1';
+const port = 3000;
+let result;
 
-http.createServer((request, response) => {
-    
+http.createServer(function(request, response) {
     const queryObj = url.parse(request.url, true).query;
     response.writeHead(200, {
         'Content-Type': 'text/html; charset=utf-8'
@@ -11,10 +14,27 @@ http.createServer((request, response) => {
     const n1 = queryObj.num1;
     const n2 = queryObj.num2;
     const s = queryObj.sign;
+
+    try {
+        result = `${eval(n1 + s + n2)}`;
+    } catch (err) {
+        fs.readFile("index.html", "utf-8", function(error, data){
+        
+            data = data.replace("{result}", "error");
+            result = 0;
+            response.end(data);
+        });
+    }
     
-    response.write(`${eval(n1 + s + n2)}`);
-    response.end();
-    
-}).listen(3000, "127.0.0.1", function() {
-    console.log("Server http://localhost:3000");
+
+    fs.readFile("index.html", "utf-8", function(error, data){
+        
+        data = data.replace("{result}", result);
+        result = 0;
+        response.end(data);
+    });
+            
+}).listen(port, host, function() {
+    console.log(`Server is running on http://${host}:${port}`);
 });
+
